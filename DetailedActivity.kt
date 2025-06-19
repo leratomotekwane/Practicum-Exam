559ae74e1141c4a00f54835d03b1d5ab3050ec47
leratomotekwane <166137659+leratomotekwane@users.noreply.github.com>
2025-06-19 12:13:38 +0000

    DetailedActivity.kt

---
+++ DetailedActivity.kt
@@ -0,0 +1,249 @@
+<?xml version="1.0" encoding="utf-8"?>
+<ScrollView xmlns:android="http://schemas.android.com/apk/res/android"
+android:layout_width="match_parent"
+android:layout_height="match_parent"
+android:background="#000000">
+
+<LinearLayout
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:orientation="vertical"
+android:padding="16dp">
+
+<TextView
+android:layout_width="wrap_content"
+android:layout_height="wrap_content"
+android:text="Enter Song Details"
+android:textSize="28sp"
+android:textColor="#FFC0CB"
+android:textStyle="bold"
+android:layout_marginBottom="20dp"
+android:layout_gravity="center_horizontal"/>
+
+<TextView
+android:layout_width="wrap_content"
+android:layout_height="wrap_content"
+android:text="Song 1"
+android:textColor="#FFC0CB"
+android:textSize="20sp"
+android:layout_marginTop="10dp"/>
+<EditText
+android:id="@+id/etSongTitle1"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Song Title"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="10dp"/>
+<EditText
+android:id="@+id/etArtistName1"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Artist Name"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="10dp"/>
+<EditText
+android:id="@+id/etSongRating1"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Rating (1-5)"
+android:inputType="number"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="10dp"/>
+<EditText
+android:id="@+id/etSongComment1"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Comments"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="20dp"/>
+
+<TextView
+android:layout_width="wrap_content"
+android:layout_height="wrap_content"
+android:text="Song 2"
+android:textColor="#FFC0CB"
+android:textSize="20sp"
+android:layout_marginTop="10dp"/>
+<EditText
+android:id="@+id/etSongTitle2"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Song Title"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="10dp"/>
+<EditText
+android:id="@+id/etArtistName2"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Artist Name"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="10dp"/>
+<EditText
+android:id="@+id/etSongRating2"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Rating (1-5)"
+android:inputType="number"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="10dp"/>
+<EditText
+android:id="@+id/etSongComment2"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Comments"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="20dp"/>
+
+<TextView
+android:layout_width="wrap_content"
+android:layout_height="wrap_content"
+android:text="Song 3"
+android:textColor="#FFC0CB"
+android:textSize="20sp"
+android:layout_marginTop="10dp"/>
+<EditText
+android:id="@+id/etSongTitle3"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Song Title"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="10dp"/>
+<EditText
+android:id="@+id/etArtistName3"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Artist Name"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="10dp"/>
+<EditText
+android:id="@+id/etSongRating3"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Rating (1-5)"
+android:inputType="number"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="10dp"/>
+<EditText
+android:id="@+id/etSongComment3"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Comments"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="20dp"/>
+
+<TextView
+android:layout_width="wrap_content"
+android:layout_height="wrap_content"
+android:text="Song 4"
+android:textColor="#FFC0CB"
+android:textSize="20sp"
+android:layout_marginTop="10dp"/>
+<EditText
+android:id="@+id/etSongTitle4"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Song Title"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="10dp"/>
+<EditText
+android:id="@+id/etArtistName4"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Artist Name"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="10dp"/>
+<EditText
+android:id="@+id/etSongRating4"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Rating (1-5)"
+android:inputType="number"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="10dp"/>
+<EditText
+android:id="@+id/etSongComment4"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:hint="Comments"
+android:textColorHint="#CCCCCC"
+android:textColor="#FFFFFF"
+android:backgroundTint="#FFC0CB"
+android:layout_marginBottom="20dp"/>
+
+
+<Button
+android:id="@+id/btnSave"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:text="Save Song Details"
+android:backgroundTint="#FFC0CB"
+android:textColor="#000000"
+android:layout_marginBottom="10dp"/>
+
+<Button
+android:id="@+id/btnClear"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:text="Clear All Data"
+android:backgroundTint="#FFC0CB"
+android:textColor="#000000"
+android:layout_marginBottom="10dp"/>
+
+<Button
+android:id="@+id/btnViewPlaylist"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:text="View Playlist"
+android:backgroundTint="#FFC0CB"
+android:textColor="#000000"
+android:layout_marginBottom="10dp"/>
+
+<Button
+android:id="@+id/btnSecondScreen"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:text="Go to Second Screen"
+android:backgroundTint="#FFC0CB"
+android:textColor="#000000"
+android:layout_marginBottom="10dp"/>
+
+<Button
+android:id="@+id/btnExitApp"
+android:layout_width="match_parent"
+android:layout_height="wrap_content"
+android:text="Exit App"
+android:backgroundTint="#800000"
+android:textColor="#FFFFFF"/>
+
+</LinearLayout>
+</ScrollView>
